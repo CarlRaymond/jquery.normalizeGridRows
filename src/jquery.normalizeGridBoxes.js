2e640bcc284308a7343a6fadc539e34401515586
Carl Raymond <carl@orcharddev.com>
2016-03-26 18:51:33 -0400

    Tabs

--- src/jquery.normalizeGridBoxes.js
+++ src/jquery.normalizeGridBoxes.js
@@ -13,13 +13,13 @@
 // The defaults are { events: [], delay: 250 }, meaning no window events are handled,
 // and the debounce delay is 250ms.
 (function (factory) {
-    
-    // Register as a module in a module environment, or as a plain jQuery
-    // plugin in a bare environment.
-    if(typeof module === "object" && typeof module.exports === "object") {
-        // CommonJS environment
-        factory(require("jquery"));
-    }
+
+	// Register as a module in a module environment, or as a plain jQuery
+	// plugin in a bare environment.
+	if(typeof module === "object" && typeof module.exports === "object") {
+		// CommonJS environment
+		factory(require("jquery"));
+	}
 	else if (typeof define === 'function' && define.amd) {
 		// AMD environment. Register as an anonymous module.
 		define(['jquery'], factory);
@@ -28,13 +28,13 @@
 		factory(jQuery);
 	}
 }(function($) {
-    
-    // This function does the work on the jQuery collection
-    // supplied as an argument. Iterates the collection (assumed to be
-    // in document order), finding elements that share the same
-    // top offset, and therefore form a row. The elements in the row
-    // are sized to the tallest member, and the process continues.
-    var normalize = function($items) {
+
+	// This function does the work on the jQuery collection
+	// supplied as an argument. Iterates the collection (assumed to be
+	// in document order), finding elements that share the same
+	// top offset, and therefore form a row. The elements in the row
+	// are sized to the tallest member, and the process continues.
+	var normalize = function($items) {
 
 		// Set height auto so items assume their natural height
 		$items.css({ height: 'auto' });
@@ -43,7 +43,7 @@
 		var maxRowHeight = 0; // Max height seen so far in row
 		var currentTopOffset = 0; // Top offset of elements in current row
 
-        // Iterate the collection
+		// Iterate the collection
 		$items.each(function (index) {
 			var $item = $(this);
 			var height = $item.height();
@@ -65,8 +65,8 @@
 					// Passed end of row. Update heights of current row.
 					$items.slice(rowStart, index).height(maxRowHeight);
 
-                    // Start next row with this item. Adjusting heights in
-                    // current row may have changed this item's top offset.
+					// Start next row with this item. Adjusting heights in
+					// current row may have changed this item's top offset.
 					rowStart = index;
 					maxRowHeight = height;
 					currentTopOffset = $item.offset().top;
@@ -77,45 +77,45 @@
 		// Update final row
 		$items.slice(rowStart).height(maxRowHeight);
 	};
-    
-    // Transforms a supplied function into a debounced version. Invoking the
-    // resulting function will start a timer of 'delay' milliseconds, and
-    // then execute the target function. Subsequent invocations will reset
-    // the timer. This is useful as an event handler for rapidly repeating events
-    // like resize.
-    var debounce = function(target_fn, delay) {
-        var timer;
-        return function() {
-            clearTimeout(timer);
-            timer = setTimeout(target_fn, delay);
-        };
-    };
-    
-    // Default options for the plugin.
-    var defaults = {
-        delay: 250,     // Debounce interval, milliseconds
-        events: []      // Events to monitor
-    };
-    
-    // The plugin proper.
-    $.fn.normalizeBoxHeights = function(options) {
-        var $items = this;
-        var settings = $.extend({}, defaults, options);
-        
-        // Normalize the selection set
-        normalize($items);
-        
-        // If any events mentioned, re-normalize when they are triggered,
-        // with debouncing.
-        if (settings.events.length > 0) {
-            var renormalize = debounce(function() { normalize($items); }, settings.delay);
-            
-            $.each(settings.events, function(index, eventname) {
-               $(window).on(eventname, renormalize);
-            });
-        }
-        
-        // Allow chaining
-        return this;
-    };
+
+	// Transforms a supplied function into a debounced version. Invoking the
+	// resulting function will start a timer of 'delay' milliseconds, and
+	// then execute the target function. Subsequent invocations will reset
+	// the timer. This is useful as an event handler for rapidly repeating events
+	// like resize.
+	var debounce = function(target_fn, delay) {
+		var timer;
+		return function() {
+			clearTimeout(timer);
+			timer = setTimeout(target_fn, delay);
+		};
+	};
+
+	// Default options for the plugin.
+	var defaults = {
+		delay: 250,		// Debounce interval, milliseconds
+		events: []		// Events to monitor
+	};
+
+	// The plugin proper.
+	$.fn.normalizeBoxHeights = function(options) {
+		var $items = this;
+		var settings = $.extend({}, defaults, options);
+		
+		// Normalize the selection set
+		normalize($items);
+		
+		// If any events mentioned, re-normalize when they are triggered,
+		// with debouncing.
+		if (settings.events.length > 0) {
+			var renormalize = debounce(function() { normalize($items); }, settings.delay);
+			
+			$.each(settings.events, function(index, eventname) {
+				$(window).on(eventname, renormalize);
+			});
+		}
+		
+		// Allow chaining
+		return this;
+	};
 }));
